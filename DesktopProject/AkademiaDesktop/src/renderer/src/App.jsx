470e2b53ec726d01f98d1940e9cf2cd9044dcdef
Pedro3DS <pedroca2787@gmail.com>
2024-05-17 19:11:50 -0400

    Update App.jsx

--- DesktopProject/AkademiaDesktop/src/renderer/src/App.jsx
+++ DesktopProject/AkademiaDesktop/src/renderer/src/App.jsx
@@ -1,23 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container'
+import Stack from 'react-bootstrap/Stack'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import InputGroup from 'react-bootstrap/InputGroup'
 import Image from 'react-bootstrap/Image'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
     <>
-      <Row style={{width: "100.8%"}}>
-        <Col style={{backgroundColor:"yellow", height: "100vh"}}>
-          <Image></Image>
-        </Col>
-        <Col style={{backgroundColor:"black", height: "100vh"}}>
-          <InputGroup>
-            <input type="text" />
-          </InputGroup>
-        </Col>
-      </Row>
+      <div className="d-flex justify-content-between align-items-center">
+        <div className='w-100 bg-warning ' style={{height:"100vh"}}>a</div>
+        <div className='w-100 bg-black' style={{height:"100vh"}}>
+          <div className="container d-flex jutify-content-center align-items-center">
+
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label className='text-light'>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-warning">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
