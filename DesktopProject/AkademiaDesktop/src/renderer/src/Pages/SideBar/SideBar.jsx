80ba83a6a64a15c828bd612ba6430eafc53e66a4
jotafreitas <joaocarlofreitas@hotmail.com>
2024-06-07 23:45:23 -0400

    opa agora sim

--- DesktopProject/AkademiaDesktop/src/renderer/src/Pages/SideBar/SideBar.jsx
+++ DesktopProject/AkademiaDesktop/src/renderer/src/Pages/SideBar/SideBar.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
-import "../../Pages/SideBar/style.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './SideBar.css';
 
 const Sidebar = () => {
+    const location = useLocation();
+    
     return (
-        <Nav className="flex-column sidebar" style={{ height: '100vh', borderRight: '1px solid #ddd', padding: '10px' }}>
-            <Nav.Link as={Link} to="/homeAdm" className="sidebar-link">Dashboard</Nav.Link>
-            <Nav.Link as={Link} to="/alunosCadastrados" className="sidebar-link">Alunos Cadastrados</Nav.Link>
-            <Nav.Link as={Link} to="/" className="sidebar-link" >Sair</Nav.Link>
-            
+        <Nav className="flex-column sidebar">
+            <Nav.Link
+                as={Link}
+                to="/homeAdm"
+                className={`sidebar-link ${location.pathname === '/homeAdm' ? 'active' : ''}`}
+            >
+                Dashboard
+            </Nav.Link>
+            <Nav.Link
+                as={Link}
+                to="/alunosCadastrados"
+                className={`sidebar-link ${location.pathname === '/alunosCadastrados' ? 'active' : ''}`}
+            >
+                Alunos Cadastrados
+            </Nav.Link>
+            <Nav.Link
+                as={Link}
+                to="/"
+                className={`sidebar-link ${location.pathname === '/' ? 'active' : ''}`}
+            >
+                Sair
+            </Nav.Link>
         </Nav>
     );
 };
