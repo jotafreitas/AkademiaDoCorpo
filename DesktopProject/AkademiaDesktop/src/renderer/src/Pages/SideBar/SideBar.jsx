50a0912963f1f6bb1a72d4405205bff08e9c76be
jotafreitas <joaocarlofreitas@hotmail.com>
2024-06-07 23:42:14 -0400

    SideBar estilizado

--- DesktopProject/AkademiaDesktop/src/renderer/src/Pages/SideBar/SideBar.jsx
+++ DesktopProject/AkademiaDesktop/src/renderer/src/Pages/SideBar/SideBar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
+import "../../Pages/SideBar/style.css"
 
 const Sidebar = () => {
     return (
-        <Nav className="flex-column" style={{ height: '100vh', borderRight: '1px solid #ddd', padding: '10px' }}>
-            <Nav.Link as={Link} to="/homeAdm">Dashboard</Nav.Link>
-            <Nav.Link as={Link} to="/alunosCadastrados">Alunos Cadastrados</Nav.Link>
-            <Nav.Link as={Link} to="/">Sair</Nav.Link>
+        <Nav className="flex-column sidebar" style={{ height: '100vh', borderRight: '1px solid #ddd', padding: '10px' }}>
+            <Nav.Link as={Link} to="/homeAdm" className="sidebar-link">Dashboard</Nav.Link>
+            <Nav.Link as={Link} to="/alunosCadastrados" className="sidebar-link">Alunos Cadastrados</Nav.Link>
+            <Nav.Link as={Link} to="/" className="sidebar-link" >Sair</Nav.Link>
+            
         </Nav>
     );
 };
