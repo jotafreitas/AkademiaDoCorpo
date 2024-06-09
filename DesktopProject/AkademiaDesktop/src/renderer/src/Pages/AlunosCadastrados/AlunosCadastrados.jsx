432be0b9cfc2ef9afecc3042b069b07600dafa25
jotafreitas <joaocarlofreitas@hotmail.com>
2024-06-09 01:09:10 -0400

    SideBar acompanha o scroll da tela

--- DesktopProject/AkademiaDesktop/src/renderer/src/Pages/AlunosCadastrados/AlunosCadastrados.jsx
+++ DesktopProject/AkademiaDesktop/src/renderer/src/Pages/AlunosCadastrados/AlunosCadastrados.jsx
@@ -10,6 +10,15 @@ function AlunosCadastrados() {
     const [alunos, setAlunos] = useState([
         { id: 1, nome: 'João Silva', pagamento: '2024-06-10' },
         { id: 2, nome: 'Maria Oliveira', pagamento: '2024-06-15' },
+        { id: 2, nome: 'Maria Oliveira', pagamento: '2024-06-15' },
+        { id: 2, nome: 'Maria Oliveira', pagamento: '2024-06-15' },
+        { id: 2, nome: 'Maria Oliveira', pagamento: '2024-06-15' },
+        { id: 2, nome: 'Maria Oliveira', pagamento: '2024-06-15' },
+        { id: 2, nome: 'Maria Oliveira', pagamento: '2024-06-15' },
+        { id: 2, nome: 'Maria Oliveira', pagamento: '2024-06-15' },
+        { id: 2, nome: 'Maria Oliveira', pagamento: '2024-06-15' },
+        { id: 2, nome: 'Maria Oliveira', pagamento: '2024-06-15' },
+        { id: 2, nome: 'Maria Oliveira', pagamento: '2024-06-15' },
         { id: 3, nome: 'Pedro Santos', pagamento: '2024-06-20' }
     ]);
 
